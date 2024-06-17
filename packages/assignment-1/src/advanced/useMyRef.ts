be95de3b441d75e73edd45b83ce5924f5bece28b
seadonggyun4 <seadonggyun@gmail.com>
2024-06-17 11:06:29 +0900

    #1, 1주차 STEP2 풀이

--- packages/assignment-1/src/advanced/useMyRef.ts
+++ packages/assignment-1/src/advanced/useMyRef.ts
@@ -1,3 +1,6 @@
+import { useState } from "react";
+
 export function useMyRef<T>(initValue: T | null) {
-  return { current: initValue }
+  const [ref] = useState<{ current: T | null }>({ current: initValue });
+  return ref;
 }
